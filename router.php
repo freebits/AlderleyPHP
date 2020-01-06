0ae9e37cbb7ee0ab7fa9dccec3e44440cc3f4d18
trader <freebits@users.noreply.github.com>
2020-01-06 18:53:41 +1000

    update

--- router.php
+++ router.php
@@ -4,17 +4,15 @@ function route($routes) {
 	$request_uri = $_SERVER["REQUEST_URI"];
 	$request_method = $_SERVER["REQUEST_METHOD"];
 	
-	foreach($routes as $route) {
-		
+	foreach($routes as $route) {		
 		list($method, $regex, $handler) = $route;
-
+		
 		if($method === $request_method &&
 			preg_match($regex, $request_uri, $params) === 1) {
-
+			
 			call_user_func($handler, $params);
 			break;
 		}
-
 	}
 }
 
