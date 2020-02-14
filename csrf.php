fff8ebd8aa5f7f697a5d77dd58fb5ba8767e1314
trader <freebits@users.noreply.github.com>
2020-02-14 17:11:35 +1000

    upated csrf

--- csrf.php
+++ csrf.php
@@ -1,15 +1,16 @@
 <?php
-session_start();
 function new_csrf_token() {
+	session_start();
 	$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
 function get_csrf_token() {
+	session_start();
 	return $_SESSION['csrf_token'];
 }
 
 function check_csrf_token($token) {
+	session_start();
 	return hash_equals($_SESSION['csrf_token'], $token);
 }
-
 ?>
