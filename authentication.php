982a5dd12961b95e02b29e90d31649579e199bc4
trader <freebits@users.noreply.github.com>
2020-02-14 16:18:45 +1000

    added pw generator

--- authentication.php
+++ authentication.php
@@ -5,4 +5,16 @@ function check_signed_in() {
 		header('location: /sign_in.php');
 	}
 }
+
+function generate_key($length = 32) {
+	$keyspace = 
+		'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[]{};:,.<>/?';
+	$keyspace_size = strlen($keyspace);
+	$key = '';
+	for(i = 0; i < $keyspace_size; i++) {
+		$key .= random_int(0, $keyspace_size);
+	}	
+	return $key;
+}
+
 ?>
