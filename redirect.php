b530b63a4fb6605b6bab70abe265e1831752e66b
trader <freebits@users.noreply.github.com>
2020-02-14 17:03:44 +1000

    update

--- redirect.php
+++ redirect.php
@@ -2,4 +2,8 @@
 function redirect($uri) {
 	header('Location: '.$uri);	
 }
+
+function x_accel_redirect($uri) {
+	header('X-Accel-Redirect: '.$uri);
+}
 ?>
--- x_accel_redirect.php
+++ x_accel_redirect.php
@@ -1,5 +1,3 @@
 <?php
-function x_accel_redirect($uri) {
-	header('X-Accel-Redirect: '.$uri);
-}
+
 ?>
