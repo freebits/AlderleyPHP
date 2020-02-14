f986ad0af2be0950c64776825d7ddd81f5aecd89
trader <freebits@users.noreply.github.com>
2020-02-14 15:44:43 +1000

    updates

--- configuration.php
+++ configuration.php
@@ -1,10 +1,11 @@
 <?php
-function configuration() {
+function get_configuration() {
 	return array(
 		'DOMAIN_NAME' => '',
 		'DATABASE_URI' => '',
 		'DATABASE_USER' => '',
 		'EMAIL_TO' => '',
-		'EMAIL_FROM' => '');
+		'EMAIL_FROM' => ''
+		'UPLOADS' => '');
 }
 ?>
