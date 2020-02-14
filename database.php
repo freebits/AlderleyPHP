67190ded22f18a71d62ae3f5b95b02d6e9480631
trader <freebits@users.noreply.github.com>
2020-02-14 17:36:38 +1000

    updates

--- database.php
+++ database.php
@@ -2,8 +2,8 @@
 require_once('configuration.php');
 function get_database() {
 	$configuration = get_configuration();
-	$dbh = 
-		new PDO($configuration['DATABASE_URI'], $configuration['DATABASE_USER']);
+	$dbh = new PDO($configuration['DATABASE_URI'],
+		$configuration['DATABASE_USER']);
   return $dbh;
 }
 ?>
--- image.php
+++ image.php
@@ -1,15 +1,16 @@
 <?php
+include_once('configuration.php');
+
 function convert_image($image) {
-	$filename_md = "{$image}_md";
-	$dir = dirname(__DIR__, 2);
-	$image_command = "convert {$dir}/admin/{$image} -strip -quality %85 -resize 1280X720 {$dir}/admin/{$filename_md}";
-	exec($image_command);
+	$configuration = get_configuration();
+	$uploads = $configuration['UPLOADS'];
+	exec("convert {$uploads}{$image} -strip -quality %85 -resize 1280X720 {$uploads}{$image}_md";
+
 }
 
 function create_thumbnail($image) {
-	$filename_thumb = "{$image}_thumb";
-	$dir = dirname(__DIR__, 2);
-	$crop_resize = "convert {$dir}/admin/{$image}_md -resize 197X150 -gravity north -crop 197X150+0+0 {$dir}/admin/{$filename_thumb}";
-	exec($crop_resize);
+	$configuration = get_configuration();
+	$uploads = $configuration['UPLOADS'];
+	exec("convert {$uploads}{$image}_md -resize 197X150 -gravity north -crop 197X150+0+0 {$uploads}{$image}_thumb";
 }
 ?>
