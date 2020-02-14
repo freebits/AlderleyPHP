cf2f668c7c9982cb99a38c6f18d3092ee6b60ef2
trader <freebits@users.noreply.github.com>
2020-02-14 15:28:07 +1000

    updated mail

--- mail.php
+++ mail.php
@@ -5,12 +5,13 @@ function contact_mail($name, $phone, $email, $postcode, $message) {
 	$configuration = get_configuration();
 	$to = configuration['EMAIL_TO'];
 	$subject = 'Contact from: '. $name;
-	$body = 
-		'Name: '. $name.PHP_EOL.
-		'Phone: '. $phone.PHP_EOL.
-		'Email: '. $email.PHP_EOL.
-		'Postcode: '. $postcode.PHP_EOL.
-		'Message: '.PHP_EOL.$message.PHP_EOL;
+	$body =
+	  'Domain: '.$configuration['DOMAIN'].PHP_EOL.
+		'Name: '.$name.PHP_EOL.
+		'Phone: '.$phone.PHP_EOL.
+		'Email: '.$email.PHP_EOL.
+		'Postcode: '.$postcode.PHP_EOL.
+		'Message: '.PHP_EOL.wordwrap($message, 80, '\n', FALSE).PHP_EOL;
 
 	$headers = 'From: '.$configuration['MAIL_FROM']; 
 	mail($to, $subject, $body, $headers);
