8f3e10abbbbd932fe369ceed2ccfb685091b1b3b
trader <freebits@users.noreply.github.com>
2020-02-14 15:13:57 +1000

    updates

--- authentication.php
+++ authentication.php
@@ -1,10 +1,8 @@
 <?php
-session_start();
-if(empty($_SESSION['auth'])) {
-  $_SESSION['auth'] = FALSE;
-}
-
-if(!$_SESSION['auth']) {
-  header('Location: /sign_in.php');
+function check_signed_in() {
+	session_start();
+	if(empty($_SESSION['auth'])) {
+		header('Location: /sign_in.php');
+	}
 }
 ?>
