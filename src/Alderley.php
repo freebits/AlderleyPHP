27a929c9bf7f6d4bc857269fc35d0a1f84893928
trader <freebits@users.noreply.github.com>
2020-04-07 11:00:49 +1000

    update

--- src/Alderley.php
+++ src/Alderley.php
@@ -2,7 +2,7 @@
 
 class Alderley {
 
-	function authentication_required()
+	public static function authentication_required()
 	{
 			session_start();
 			if (empty($_SESSION['auth'])) {
@@ -11,21 +11,21 @@ class Alderley {
 	}
 
 
-	function authenticate()
+	public static function authenticate()
 	{
 			session_start();
 			$_SESSION['auth'] = true;
 	}
 
 
-	function deauthenticate()
+	public static function deauthenticate()
 	{
 			session_start();
 			unset($_SESSION['auth']);
 	}
 
 
-	function generate_password(int $password_length)
+	public static function generate_password(int $password_length)
 	{
 
 			$keyspace = array(
@@ -46,40 +46,40 @@ class Alderley {
 	}
 
 
-	function get_configuration(string $cfg_file_path)
+	public static function get_configuration(string $cfg_file_path)
 	{
 			return parse_ini_file($cfg_file_path);
 	}
 
 
-	function new_csrf_token()
+	public static function new_csrf_token()
 	{
 			session_start();
 			$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 	}
 
 
-	function get_csrf_token()
+	public static function get_csrf_token()
 	{
 			session_start();
 			return $_SESSION['csrf_token'];
 	}
 
 
-	function check_csrf_token(string $token)
+	public static function check_csrf_token(string $token)
 	{
 			session_start();
 			return hash_equals($_SESSION['csrf_token'], $token);
 	}
 
 
-	function get_database(string $db_uri, string $db_user)
+	public static function get_database(string $db_uri, string $db_user)
 	{
 			return new PDO($db_uri, $db_user);
 	}
 
 
-	function resize_image(string $image_in, string $image_out, int $cols, int $rows)
+	public static function resize_image(string $image_in, string $image_out, int $cols, int $rows)
 	{
 			$image = new Imagick($image_in);
 			$image->adaptiveResizeImage($cols, $rows, true);
@@ -88,7 +88,7 @@ class Alderley {
 	}
 
 
-	function thumbnail_image(string $image_in, string $image_out, int $cols, int $rows)
+	public static function thumbnail_image(string $image_in, string $image_out, int $cols, int $rows)
 	{
 			$image = new Imagick($image_in);
 			$image->thumbnailImage($cols, $rows, true);
@@ -97,31 +97,31 @@ class Alderley {
 	}
 
 
-	function sanitize_input(string $i)
+	public static function sanitize_input(string $i)
 	{
 			return htmlspecialchars(stripslashes(trim($i)));
 	}
 
 
-	function sanitize_string(string $s)
+	public static function sanitize_string(string $s)
 	{
 			return filter_var(sanitize_input($s), FILTER_SANITIZE_STRING);
 	}
 
 
-	function sanitize_integer(int $i)
+	public static function sanitize_integer(int $i)
 	{
 			return filter_var(sanitize_input($i), FILTER_SANITIZE_NUMBER_INT);
 	}
 
 
-	function sanitize_email(string $e)
+	public static function sanitize_email(string $e)
 	{
 			return filter_var(sanitize_input($e), FILTER_SANITIZE_EMAIL);
 	}
 
 
-	function contact_mail($mail_to, $mail_from, $subject, $fields)
+	public static function contact_mail(string $mail_to, string $mail_from, string $subject, array $fields)
 	{
 			$body = '';
 			foreach ($fields as $field) {
@@ -133,25 +133,25 @@ class Alderley {
 	}
 
 
-	function get_pagination_offset($page, $limit = 9)
+	public static function get_pagination_offset(int $page, int $limit = 9)
 	{
 			return ($page - 1) * $limit;
 	}
 
 
-	function redirect($uri)
+	public static function redirect(string $uri)
 	{
 			header('Location: '.$uri);
 	}
 
 
-	function x_accel_redirect($uri)
+	public static function x_accel_redirect(string $uri)
 	{
 			header('X-Accel-Redirect: '.$uri);
 	}
 
 
-	function create_slug($s)
+	public static function create_slug(string $s)
 	{
 			return str_replace(
 					" ",
@@ -159,5 +159,4 @@ class Alderley {
 					strtolower(preg_replace("/[^0-9a-zA-Z ]/", "", $s))
 			);
 	}
-
 }
