72395c76c97ee6f438c90120dcff2ae15a042229
trader <freebits@users.noreply.github.com>
2020-04-08 09:48:17 +1000

    update

--- src/AlderleyRouter.php
+++ src/AlderleyRouter.php
@@ -6,14 +6,14 @@ class AlderleyRouter {
 
     public function route()
     {
-        $request_uri = $_SERVER["REQUEST_URI"];
-        $request_method = $_SERVER["REQUEST_METHOD"];
+        $requestUri = $_SERVER["REQUEST_URI"];
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
         
         foreach ($this->routes as $route) {
-            list($method, $regex, $callback) = $route;
+            list($httpVerb, $regex, $callback) = $route;
             
-            if ($method === $request_method &&
-                preg_match($regex, $request_uri, $params) === 1) {
+            if ($method === $requestMethod &&
+                preg_match($regex, $requestUri, $params) === 1) {
                 call_user_func($callback, $params);
                 break;
             }
@@ -23,7 +23,7 @@ class AlderleyRouter {
     public function addRoute(string $httpVerb, string $regex, string $callback)
     {
         $route = array(
-            0 => $http_verb,
+            0 => $httpVerb,
             1 => $regex,
             2 => $callback
         );
