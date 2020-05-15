121279f87fe7bd601e62ed49f61762a63bf1a968
trader <freebits@users.noreply.github.com>
2020-05-15 11:33:03 +1000

    update

--- src/MailGun.php
+++ src/MailGun.php
@@ -4,20 +4,20 @@ namespace AlderleyPHP;
 
 class MailGun
 {
-    public static function sendMail($key, $to, $from, $subject, $text)
+    public static function sendMail($key, $url, $to, $from, $subject, $text)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$key);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/mail.coolteam.com.au/messages');
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POSTFIELDS, array(
             'to' => $to,
             'from' => $from,
             'subject' => $subject,
             'text' => $text));
-        curl_exec($ch);
+        $json_result = curl_exec($ch);
         curl_close($ch);
         return;
     }
