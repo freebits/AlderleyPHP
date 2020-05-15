8870e297eea179377f1c3ae62c5d8463508728c2
trader <freebits@users.noreply.github.com>
2020-05-15 11:38:04 +1000

    update

--- src/MailGun.php
+++ src/MailGun.php
@@ -4,14 +4,14 @@ namespace AlderleyPHP;
 
 class MailGun
 {
-    public static function sendMail($key, $url, $to, $from, $subject, $text): void
+    public static function sendMail($mgKey, $mgUrl, $to, $from, $subject, $text): void
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$key);
+        curl_setopt($ch, CURLOPT_USERPWD, 'api:'.$mgKey);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_URL, $mgUrl);
         curl_setopt($ch, CURLOPT_POSTFIELDS, array(
             'to' => $to,
             'from' => $from,
