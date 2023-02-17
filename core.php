4519ad01f569a7fd99534ca757f9a2a4674af7ef
Free Bits <freebits@users.noreply.github.com>
2023-02-17 15:49:31 +1000

    update

--- core.php
+++ core.php
@@ -2,12 +2,19 @@
 
 class Skeleton {
     
+    protected string $configuration; 
+    
+    function __construct(string $configuration) {
+        $this->$configuration = parse_ini_file($configuration); 
+    }
+
     public static function GetDatabase() {
         $connection = null;
         try {
-            $connection = new PDO()
+            $connection = new PDO($this->configuration["DATABASE_CONNECTION_STRING"]);
         }
         catch(PDOException $e) {
+            error_log($e); 
         }
         return $connection;
     }
