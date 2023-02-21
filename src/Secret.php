5f1217bd0fb90ab35a11307f1ca951a3fdefa6c7
Free Bits <freebits@users.noreply.github.com>
2023-02-22 09:43:21 +1000

    update

--- src/Secret.php
+++ src/Secret.php
@@ -40,58 +40,15 @@ class Secret
         return $connection;
     }
 
-    public function mail_gun_send(string $to, string $from, string $subject, string $message): void
+    public function generate_password(int $password_length): string
     {
-        $ch = curl_init();
-        if($ch === false)
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        $number_of_chars = strlen($chars);
+        $results = "";
+        for($i=0; $i < $password_length; $i++)
         {
-            error_log("Could not initialize curl");
+            $results .= $chars[random_int(0, $number_of_chars)];
         }
-
-        $configuration = $this->get_configuration();
-        $mailgun_secret_key = $configuration['MAILGUN_SECRET_KEY'];
-        $mailgun_domain = $configuration['MAILGUN_DOMAIN'];
-
-        if(!curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC))
-        {
-            error_log(curl_error($ch));
-        }
-
-        if(!curl_setopt($ch, CURLOPT_USERPWD, 'api:' . $mailgun_secret_key))
-        {
-            error_log(curl_error($ch));
-        }
-
-        if(!curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1))
-        {
-            error_log(curl_error($ch));
-        }
-
-        $plain = strip_tags(nl2br($message));
-
-        if(!curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'))
-        {
-            error_log(curl_error($ch));
-        }
-
-        $mailgun_url = 'https://api.mailgun.net/v2/' . $mailgun_domain . '/messages';
-        if(!curl_setopt($ch, CURLOPT_URL, $mailgun_url))
-        {
-            error_log(curl_error($ch));
-        }
-
-        $curl_post_fields = array(
-            'from' => $from,
-            'to' => $to,
-            'subject' => $subject,
-            'text' => $plain);
-
-        if(!curl_setopt($ch, CURLOPT_POSTFIELDS, $curl_post_fields))
-        {
-            error_log(curl_error($ch));
-        }
-
-        curl_exec($ch);
-        curl_close($ch);
+        return $results;
     }
 }
